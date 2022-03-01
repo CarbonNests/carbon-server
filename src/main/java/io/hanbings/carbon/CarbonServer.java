6189470796f48d3e7698dd8113ac29a0de7d9277
hanbings <3219065882@qq.com>
2022-03-01 21:10:31 +0800

    🚧 Feat: Class scanner.

--- src/main/java/io/hanbings/carbon/CarbonServer.java
+++ src/main/java/io/hanbings/carbon/CarbonServer.java
@@ -1,8 +1,19 @@
 package io.hanbings.carbon;
 
 import io.hanbings.carbon.common.content.Carbon;
+import io.hanbings.carbon.common.util.FileUtils;
 import io.hanbings.carbon.event.EventBus;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Objects;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 @Slf4j
 public class CarbonServer {
@@ -23,8 +34,62 @@ public class CarbonServer {
          * 因为 Event 是先于处理逻辑触发的
          */
 
-        //收集类路径
+        // 收集类路径下所有的类
+        @SuppressWarnings("SpellCheckingInspection")
+        String artifact = "io.hanbings.carbon";
+        String path = CarbonServer.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        List<String> classes = new ArrayList<>();
 
+        // 遍历 String 带路径类名
+        if (path.endsWith(".jar")) {
+            try {
+                Enumeration<URL> urlEnumeration = Thread.currentThread()
+                        .getContextClassLoader().getResources(artifact.replace(".", "/"));
+                while (urlEnumeration.hasMoreElements()) {
+                    URL url = urlEnumeration.nextElement();
+                    JarURLConnection connection = (JarURLConnection) url.openConnection();
+                    JarFile jarFile = connection.getJarFile();
+                    Enumeration<JarEntry> jarEntryEnumeration = jarFile.entries();
+                    while (jarEntryEnumeration.hasMoreElements()) {
+                        JarEntry entry = jarEntryEnumeration.nextElement();
+                        String jarEntryName = entry.getName();
+                        if (jarEntryName.contains(".class") && jarEntryName.replaceAll("/", ".").startsWith(artifact)) {
+                            classes.add(jarEntryName.substring(0, jarEntryName.lastIndexOf(".")).replace("/", "."));
+                        }
+                    }
+                }
+                Thread.currentThread()
+                        .getContextClassLoader().getResources(artifact.replace(".", "/"))
+                        .asIterator().forEachRemaining(url -> {
+                            JarURLConnection connection = (JarURLConnection) url.openConnection();
+                            JarFile jarFile = connection.getJarFile();
+                            jarFile.entries().asIterator().forEachRemaining(entry -> {
+                                String jarEntryName = entry.getName();
+                                if (jarEntryName.contains(".class") && jarEntryName.replaceAll("/", ".").startsWith(artifact)) {
+                                    classes.add(jarEntryName.substring(0, jarEntryName.lastIndexOf(".")).replace("/", "."));
+                                }
+                            });
+                        });
+            } catch (IOException exception) {
+                log.error("unknown io error.", exception);
+            }
+        } else {
+            String file = Objects.requireNonNull(CarbonServer.class.getResource("/")).getPath()
+                    + artifact.replace(".", "/");
+            FileUtils.getFiles(file).stream().filter(f -> f.getName().endsWith(".class")).forEach(f -> {
+                // 千万别动 已经不认识这坨垃圾了
+                classes.add(f.getPath()
+                        .replace(Objects.requireNonNull(CarbonServer.class.getResource("/")).getPath()
+                                .replace("/", "\\")
+                                .replaceFirst("\\\\", ""), "")
+                        .replace("\\", ".")
+                        .replace(".class", ""));
+            });
+        }
+
+        classes.forEach(c -> {
+            log.info("scan class: {}", c);
+        });
 
         //bus.callEvent(new CarbonServerBootstrapEvent(new ServiceContainer()));
 
---
+++ src/main/java/io/hanbings/carbon/common/util/FileUtils.java
@@ -0,0 +1,33 @@
+package io.hanbings.carbon.common.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class FileUtils {
+    public static List<File> getFiles(String path) {
+        List<File> files = new ArrayList<>();
+        FileUtils.addFile(new File(path), files);
+        return files;
+    }
+
+    private static void addFile(File file, List<File> files) {
+        // Todo: 什么时候回来修缮下 改成流处理
+        if (file.isFile()) {
+            files.add(file);
+        } else {
+            File[] fs = file.listFiles();
+            if (fs != null) {
+                for (File f : fs) {
+                    addFile(f, files);
+                }
+            }
+        }
+    }
+
+}
